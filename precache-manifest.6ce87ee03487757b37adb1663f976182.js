74e620bc1e31cba88377a842335afbe271c1d23d
Thies Dieckmann <thies.dieckmann@stud.fh-flensburg.de>
2020-12-28 20:51:46 +0100

    First Deploy on GH Pages

---
+++ precache-manifest.6ce87ee03487757b37adb1663f976182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7b63caac9e060552d8",
+    "url": "/tdckmnn.github.io/css/app.1eab3563.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/tdckmnn.github.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0d4fc42a0f7a468d1f4125458b237e51",
+    "url": "/tdckmnn.github.io/index.html"
+  },
+  {
+    "revision": "6b7b63caac9e060552d8",
+    "url": "/tdckmnn.github.io/js/app.0aa67d6a.js"
+  },
+  {
+    "revision": "ca36a2f3182120e3092d",
+    "url": "/tdckmnn.github.io/js/chunk-vendors.a68d2ddc.js"
+  },
+  {
+    "revision": "e5d20ab2cf55cfdc34e76c8fbfa2aec9",
+    "url": "/tdckmnn.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tdckmnn.github.io/robots.txt"
+  }
+]);
---
+++ registerServiceWorker.js
@@ -0,0 +1,36 @@
+importScripts("/tdckmnn.github.io/precache-manifest.6ce87ee03487757b37adb1663f976182.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}registerServiceWorker.js`, {
+  // register('/registerServiceWorker.js', {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered () {
+      console.log('Service worker has been registered.')
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updatefound () {
+      console.log('New content is downloading.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
+
