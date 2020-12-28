bd6d6f1800891ffe17212ceb32c98a82ae9a7591
Thies Dieckmann <thies.dieckmann@stud.fh-flensburg.de>
2020-12-28 22:19:22 +0100

    Service Worker Fix

---
+++ precache-manifest.1909a294e9f60e40346dd06edb0ed375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c28c296efb8981a0cf",
+    "url": "/tdckmnn.github.io/css/app.1eab3563.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/tdckmnn.github.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6c98cfb1fbdd8efc063f231aa97efe55",
+    "url": "/tdckmnn.github.io/index.html"
+  },
+  {
+    "revision": "59c28c296efb8981a0cf",
+    "url": "/tdckmnn.github.io/js/app.7fa3694b.js"
+  },
+  {
+    "revision": "ca36a2f3182120e3092d",
+    "url": "/tdckmnn.github.io/js/chunk-vendors.a68d2ddc.js"
+  },
+  {
+    "revision": "e5d20ab2cf55cfdc34e76c8fbfa2aec9",
+    "url": "/tdckmnn.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tdckmnn.github.io/robots.txt"
+  }
+]);
